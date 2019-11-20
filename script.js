85a0378a2b3cde4851285c14ee49837776a2add6
netanel-code <jiraya770@gmail.com>
2019-11-20 18:57:30 +0200

    added script+paragraph

---
+++ script.js
@@ -0,0 +1,10 @@
+const array = [12 , 45 , 55 , 59];
+let bigNum = Number.MIN_SAFE_INTEGER;
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+    return bigNum += element;
+
+
+}
